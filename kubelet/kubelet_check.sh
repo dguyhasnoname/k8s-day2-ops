6481875aac6558f4ccbc9cf4c3b85a68d7df9389
Mukund Sharma <mukund_bihari@mckinsey.com>
2020-04-28 18:27:07 +0530

    fixing issue 1

--- kubelet/kubelet_check.sh
+++ kubelet/kubelet_check.sh
@@ -3,16 +3,23 @@
 # This script gives status of kubelt on each nodes of a cluster       #
 # KUBECONFIG needs to be exported as env before running the script    #
 # Author: Mukund                                                      #
-# Date: 17th August 2019                                              #
-# Version: 1.0                                                        #
+# Date: 28th April 2020                                               #
 #######################################################################
 
+BOLD='\033[1;30m'
+END='\033[0m'
+
 separator() {
-    printf "===========================================\n"
+    printf '\n'
+}
+
+indent () {
+    x="$1"
+    awk '{printf "%"'"$x"'"s%s\n", "", $0}'
 }
 
 get_nodes() {
-    kubectl get nodes --no-headers | awk '{print $1}' > NODES
+    NODES="$(kubectl get nodes -o=jsonpath='{range .items[*]}{.metadata.name}{"\n"}')"
 }
 
 get_status() {
@@ -20,10 +27,10 @@ get_status() {
     while read -r line;
     do
         separator
-        printf "kubelet status of node $line\n"
-        echo "-------------------------------------------"
-        kubectl get node "$line" -o json| jq '.status.conditions[].message'
-    done < "NODES"
+        echo -e "${BOLD}kubelet status of node $line:\n${END}"
+        kubectl get node "$line" \
+        -o=jsonpath='{range .status.conditions[*]}{.message}{"\n"}{end}' | indent 8
+    done <<< "$NODES"
 }
 
 get_status
