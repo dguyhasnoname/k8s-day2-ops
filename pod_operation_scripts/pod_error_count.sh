06e6609ace5f9c81143fddea5a8ff9683eb0e64c
dguyhasnoname <dguyhasnoname@gmail.com>
2020-11-25 08:48:11 +0530

    updated error count script to count any specific string

--- pod_operation_scripts/pod_error_count.sh
+++ pod_operation_scripts/pod_error_count.sh
@@ -1,7 +1,7 @@
-#!/bin/bash -e
+#!/bin/bash 
 #######################################################################
-# This script greps for case-insentitive 'error' string in a pod over #
-# the last 5 minutes. It can optionally add additional param like     #
+# This script greps for case-insentitive string in a pod over         #
+# the last given minutes. It can optionally & additional param like   #
 # '10m' or '24h' to change the --TIME= value. Default ns is kube-     #
 # system. This script needs KUBECONFIG to be exported prior to run it.#
 # Author: Mukund                                                      #
@@ -9,60 +9,91 @@
 # Version: 1.0                                                        #
 #######################################################################
 
-POD_NAME="$1"
-TIME="${2:-5m}"
-NAMESPACE="${3:-kube-system}"
-STRING="$4"
+START_TIME=$(date +%s)
+YELLOW='\033[1;33m'
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+BOLD='\033[1;30m'
+TICK='\xE2\x9C\x94'
+NOT_OK='\xE2\x9D\x8C'
+END='\033[0m'
 
 usage() {
-    echo "[WARNINIG]: It may be a invalid sytanx!"
-    echo "Usage: pod_error_count.sh <pod_names_to_grep> <optional time(5m or 2h); default 5m> <namespace, default vaules is kube-system>  <optional search string>."
-    echo "e.g pod_error_count.sh  kube-proxy 24h kube-system invalid"
+    echo "[WARNING] It may be a invalid sytanx!"
+    echo "Usage: pod_error_count.sh -p <pod_names_to_grep> -t <optional time(5m or 2h); default 5m> -n <namespace, default vaules is kube-system>  -s <optional search string>."
+    echo "e.g pod_error_count.sh -p kube-proxy -t 24h -n kube-system -s invalid"
     separator
     exit
 }
 
 separator() {
-    echo "------------------------------------------------------------------------------------------------------------------"
+    printf "\n\n"
 }
 
 pod_list(){
     separator
+    echo -e "${GREEN}[RUNNING]${END} kubectl -n "$NAMESPACE" get pods --no-headers | grep -i "$POD_NAME" "
     POD_LIST="$(kubectl -n "$NAMESPACE" get pods --no-headers | grep -i "$POD_NAME" |  awk '{print $1}')"
 
     if [[ "$POD_LIST" == "" ]];
     then
-        echo "[WARNINIG]: Pod $POD_NAME not found in namespace $NAMESPACE"
-        usage
+        echo -e "${YELLOW}[WARNING]${END} Pod names with containing string \"$POD_NAME\" not found in namespace $NAMESPACE!"
         separator
         exit
     else
-        true
+        echo -e "${GREEN}Pods found:${END}" 
+        echo "$POD_LIST"
     fi
-}
-
-get_count() {
-    [ -z "$POD_NAME" ] && usage
-    pod_list
-    printf "fetching error count in namespace for below pods in last $TIME...\n\n"
-    while IFS= read -r line;
-    do
-        ERROR_COUNT=$(kubectl -n "$NAMESPACE" logs "$line" --since="$TIME" | grep -aci error)
-        echo "Pod = $line, Error count = $ERROR_COUNT"
-    done <<< "$POD_LIST"
-    separator
 }
 
 grep_error() {
     [ -z "$STRING" ] && get_count && exit
-    printf "searching for string $STRING in logs for below pods in namespace $NAMESPACE in last $TIME...\n\n"
+    separator
+    printf "searching for string $STRING in logs for pods $POD_NAME in namespace $NAMESPACE in last $TIME..."
     pod_list
+    
     while IFS= read -r line;
     do
-        ERROR_COUNT=$(kubectl -n "$NAMESPACE" logs "$line" --since="$TIME" | grep -aci "$STRING")
-        echo "Pod = $line, Error count for errors related to string $STRING = $ERROR_COUNT"
+        separator
+        echo -e "${GREEN}[RUNNING]${END} kubectl -n $NAMESPACE logs $line --since=$TIME | grep -aci $STRING"
+        MATCHES_COUNT=$(kubectl -n "$NAMESPACE" logs "$line" --since="$TIME" | grep -aci "$STRING")
+        echo -e "${BOLD}Pod:${END} $line" 
+        echo -e "${BOLD}Count${END} of matches for string \"$STRING\" in logs: ${BOLD}$MATCHES_COUNT${END}"
     done <<< "$POD_LIST"
     separator
 }
 
+OPTIND=1         
+
+while getopts "h?n:s:p:t:" opt; do
+    case "$opt" in
+    h|\?)
+        usage
+        exit 0
+        ;;
+    n)  NAMESPACE=$OPTARG
+        ;;   
+    s)  STRING=$OPTARG 
+        ;;
+    p)  POD_NAME=$OPTARG
+        ;;
+    t)  TIME=$OPTARG
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+[ "${1:-}" = "--" ] && shift
+
+[[ -z "$NAMESPACE"  || -z "$STRING" ]] && \
+echo "[ERROR] Missing mandatory arguments" && usage 
+[[ "$TIME" == "" ]] && TIME='5m'
+[[ "$NAMESPACE" == "" ]] && NAMESPACE='kube-system'
+[[ "$STRING" == "" ]] && STRING='error'
+
 grep_error
+
+END_TIME=$(date +%s)
+EXECUTION_TIME=$((END_TIME-START_TIME))
+echo "Total time taken:" "$EXECUTION_TIME"s
